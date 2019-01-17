cb50c969787cb789433e168cc6fad03d793cbd9c
LindaMoussa <linda.moussa.95@gmail.com>
2019-01-17 20:46:08 +0200

    InvitationCards_V4

--- js/home.js
+++ js/home.js
@@ -14,13 +14,15 @@ $(window).scroll(function(){
     $(".nevs li a").css("color","#000");
     $('.form-inline').css("border-left","rgba(0,0,0,0.5) 1px solid");
     $('.logo').attr("src","images/darklogo.png");
-        
+    $('.profileIcon a i').css("color","000") ;  
     }
     else{
         
     $(".nevs").css("backgroundColor","rgba(0,0,0,.1)");
     $(".nevs li a").css("color","#fff");
-    $('.form-inline').css("border-left","rgba(250,250,250,0.5) 1px solid"); $('.logo').attr("src","images/WhiteLogolastttt.png");
+    $('.form-inline').css("border-left","rgba(250,250,250,0.5) 1px solid"); 
+    $('.logo').attr("src","images/WhiteLogolastttt.png");
+        $('.profileIcon a i').css("color","#fff") ; 
     }
 
 })
--- js/savedImages.js
+++ js/savedImages.js
@@ -4,11 +4,14 @@ var row=document.getElementById("savedCardsRow");
 var cont="";    
       console.log("Local Storage Len "+localStorage.length);
     for (var i=0;i<localStorage.length;i++)
+        { if(localStorage.key(i).includes("savedimg"))
         {
-            var image =localStorage.getItem(localStorage.key(i));
-//            console.log(image);
-//            localStorage.getItem('savedimg'+i);
-           cont+=" <div class='col-md-4 my-2'><img  id='img"+(i)+"' class='img-fluid w-75' src="+image+" /><p class='mt-3'><a href='javascript:void(0)'onclick='removeCard("+i+")'>Remove</a></p></div>"    
+              var image =localStorage.getItem(localStorage.key(i));
+              console.log(typeof(localStorage.key(i)));
+    //            console.log(image);
+    //            localStorage.getItem('savedimg'+i);
+               cont+=" <div class='col-md-4 my-2'><img  id='img"+(i)+"' class='img-fluid w-75' src="+image+" /><p class='mt-3'><a href='javascript:void(0)'onclick='removeCard("+i+")'>Remove</a></p></div>"    
+            }
         }
     
     row.innerHTML=cont;
@@ -22,3 +25,86 @@ function removeCard(idx){
     localStorage.removeItem(localStorage.key(idx));
     displayCards();
 }
+if(localStorage.getItem('UserName'))
+    {
+        $('#name').html(localStorage.getItem('UserName'));
+    }
+if(localStorage.getItem('UserEmail'))
+    {
+        $('#email').html(localStorage.getItem('UserEmail'));
+    }
+if(localStorage.getItem('UserBio'))
+    {
+        $('#bio').html(localStorage.getItem('UserBio'));
+    }
+$("#editBtnName").click(function(){
+    
+    $("#editUserName").css("display","block");
+    $("#name").css("display","none");
+    $("#editBtnName").css("display","none");
+    $("#doneBtnName").css("display","block");
+    $("#doneBtnName").click(function(){
+          
+    var name = $('#editUserName').val();
+    $('#name').html(name);
+    $("#editUserName").css("display","none");
+    $("#name").css("display","block");
+    $("#editBtnName").css("display","block");
+    $("#doneBtnName").css("display","none");
+    localStorage.setItem('UserName', name);
+    })
+ 
+});
+
+$("#editBtnEmail").click(function(){
+    
+    $("#editUserEmail").css("display","block");
+    $("#email").css("display","none");
+    $("#editBtnEmail").css("display","none");
+    $("#doneBtnEmail").css("display","block");
+    $("#doneBtnEmail").click(function(){
+          
+    var email = $('#editUserEmail').val();
+    console.log(email)
+    $('#email').html(email);
+    $("#editUserEmail").css("display","none");
+    $("#email").css("display","block");
+    $("#editBtnEmail").css("display","block");
+    $("#doneBtnEmail").css("display","none");
+    localStorage.setItem('UserEmail', email);   
+    })
+ 
+});
+
+$("#editBtnBio").click(function(){
+    
+    $("#editUserBio").css("display","block");
+    $("#bio").css("display","none");
+    $("#editBtnBio").css("display","none");
+    $("#doneBtnBio").css("display","block");
+    $("#doneBtnBio").click(function(){
+          
+    var bio = $('#editUserBio').val();
+    console.log(bio)
+    $('#bio').html(bio);
+    $("#editUserBio").css("display","none");
+    $("#bio").css("display","block");
+    $("#editBtnBio").css("display","block");
+    $("#doneBtnBio").css("display","none");
+     localStorage.setItem('UserBio', bio);   
+    })
+ 
+});
+//$("#changeProfile").click(function(){
+//    $('input[type=file]').trigger('click');
+//    $('input[type=file]').change(function() {
+//    var vals = $(this).val(), val = vals.length ? vals.split('\\').pop() : '';
+//
+//    $('input[type=text]').val(val);
+//        $(".profile img").attr('src',val)
+//        var filePath = $(this).val();
+//        console.log(filePath);
+// 
+//});
+//})
+
